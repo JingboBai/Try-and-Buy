968c8267ad016b3d32638f147d2727d1c508cc36
JingboBai <billowno1@foxmail.com>
2015-11-24 14:53:25 -0800

    use session variables

--- Project updated/php/addToBag.php
+++ Project updated/php/addToBag.php
@@ -3,22 +3,26 @@ include("../html/header.html");
 ?>
 
 <?php
+    session_start();
     mysql_connect("localhost", "root", "") or die("Error connecting to database: ".mysql_error());
     mysql_select_db("try&buy_db") or die(mysql_error());
     $quantity= $_POST["quantity"];
     $ProductName= $_POST["productName"];
 
       if($quantity>= 1){
+        $_SESSION["quantity"] = "$quantity";
+        $_SESSION["ProductName"] = "$ProductName";
         $row_results = mysql_query("UPDATE Products
                                            SET Quantity='$quantity'
                                            WHERE ProductName='$ProductName'");
     }
 ?>
 
-    <div class="body-section">
+    <div class="search-section">
     <p> The thing has been added to your cart;</p>
-    <p><?=$quantity?>and<?=$ProductName?></p>
-    <div>
+    <p> Quantity is <?=$_SESSION["quantity"]?> <br>
+        Product name is <?=$_SESSION["ProductName"]?></p>
+    </div>
 
 
 <?php
